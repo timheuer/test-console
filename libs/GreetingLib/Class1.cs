24c227ac99c3c5ace1c1e680bbc8fe5c9e111acf
Tim Heuer <tim@timheuer.com>
2020-01-30 17:45:43 -0800

    Adding lib

---
+++ libs/GreetingLib/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GreetingLib
+{
+    public class Greetings
+    {
+        string[] greetings = new String[] { "Hello", "Goodbye", "G'day", "Cheers" };
+
+        public string SaySomething(string inputName)
+        {
+            Random r = new Random();
+
+            var greet = r.Next(0, greetings.Length - 1);
+
+            return $"{greet} {inputName}";
+        }
+    }
+}
